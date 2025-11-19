f00176b05f92c98808c9cdb3d573b50441ff9fa8
Bronson Yen <bronson1013@gmail.com>
2025-11-18 23:41:20 -0800

    Format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
-
 import { MainChat } from "~/components/chat";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   return (
